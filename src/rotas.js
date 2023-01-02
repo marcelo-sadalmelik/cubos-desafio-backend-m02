54460f8e908c94824a8dfcc6c41f09421bd466c5
Marcelo Pereira Bastos <marcelo.jedda@gmail.com>
2023-01-02 10:15:34 -0300

    adicionado validação da senha

--- src/rotas.js
+++ src/rotas.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const validaSenha = require('./intermediarios');
 const { buscarContas } = require('./controladores/contas');
 
 const rotas = express();
 
+rotas.use(validaSenha);
 rotas.use('/contas', buscarContas);
 
 module.exports = rotas;
