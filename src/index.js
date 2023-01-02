fe2d60359443aea632d77c01d732b535637d57d5
Marcelo Pereira Bastos <marcelo.jedda@gmail.com>
2023-01-02 10:09:29 -0300

    criado arquivo de rotas

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const rotas = require('./rotas');
 
 const app = express();
 
 app.use(express.json());
+app.use(rotas);
+
 app.listen(3000, () => {
   console.log('Servidor rodando.');
 });
